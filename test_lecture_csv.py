cfa16c66dd1de4c970f254db9d326c6a8fe5718a
Hattariyo <arthur.viot@avec.live>
2025-03-30 17:27:36 +0200

    🧹 Supprime script de debug

--- test_lecture_csv.py
+++
@@ -1,9 +0,0 @@
-with open("historique_complet.csv", "rb") as f:
-    content = f.read()
-
-for i in range(len(content)):
-    try:
-        content[:i].decode("utf-8")
-    except UnicodeDecodeError as e:
-        print(f"❌ Erreur à la position {i} : {e}")
-        break
